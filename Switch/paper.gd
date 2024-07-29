885224da13a8009e7071b61989ede0f87481ccfd
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2024-07-29 21:25:30 +0200

    new tilesets & new levels

--- Switch/paper.gd
+++ Switch/paper.gd
@@ -1,8 +1,12 @@
 extends StaticBody2D
 
 @export var dialogue_box: CanvasLayer
+@export var frame: int = 0
+
+func _ready():
+	$Sprite2D.frame = frame
 
 func flip_switch():
 	$AudioStreamPlayer2D.play()
 	dialogue_box.visible = true
-	visible = false
+	visible = false 
